7ada2a7bb9b6d3c6fc45978bb2a65df826d707f3
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-24 11:10:24 +0530

    [Rohit]Add: // add all files

---
+++ LineComparison/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class Line
+    {
+        private double x1, y1, x2, y2;
+
+        public Line(double x1, double y1, double x2, double y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public bool Equals(Line other)
+        {
+            if (other == null)
+                return false;
+
+            return x1 == other.x1 && y1 == other.y1 && x2 == other.x2 && y2 == other.y2;
+        }
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Equality Check!");
+
+            // Create two lines
+            Line line1 = new Line(2, 3, 5, 7);
+            Line line2 = new Line(2, 3, 5, 7);
+
+            // Check if the lines are equal
+            bool areEqual = line1.Equals(line2);
+
+            if (areEqual)
+            {
+                Console.WriteLine("The lines are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The lines are not equal.");
+            }
+        }
+    }
+}
